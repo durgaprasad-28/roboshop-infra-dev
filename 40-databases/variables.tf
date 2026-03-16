6acf89775a84cc0b9471fdc11a973c593ef6599e
DP <durga66352@gmail.com>
2026-03-16 17:00:55 +0530

    terraform

---
+++ 40-databases/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z01835103JCFTRB96GT29"
+}
+
+variable "domain_name" {
+    default = "durgarasad.online"
+}
